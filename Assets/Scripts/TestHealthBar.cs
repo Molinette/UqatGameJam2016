0725c27f53066e1d1841f7b22a9beec25e6a6794
Kerby Tineus <an18200@ens.etsmtl.ca>
2016-11-04 23:25:18 -0400

    Barre de vie completer

---
+++ Assets/Scripts/TestHealthBar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestHealthBar : MonoBehaviour {
+
+	private float maxDertermination;
+	public float currentVie = 20f;
+	public GameObject bar;  
+
+	// Use this for initialization
+	void Start () {
+		maxDertermination = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bar.transform.localScale = new Vector2 (bar.transform.localScale.x, currentVie / maxDertermination);
+	}
+}
