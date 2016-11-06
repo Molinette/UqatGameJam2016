b35af28ad5a6b1c56696300eadf27004d1c545e1
Kerby Tineus <an18200@ens.etsmtl.ca>
2016-11-06 15:13:38 -0500

    final

---
+++ Assets/Scripts/ScoreFinal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreFinal : MonoBehaviour {
+
+	public Text princessAmountText;
+	public string before;
+	public string after;
+
+	// Use this for initialization
+	void Start () {
+		
+		princessAmountText.text = before + " " + PlayerPrefs.GetInt ("Princess") + " "+ after ;
+
+	}
+}
