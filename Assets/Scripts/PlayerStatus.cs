3f367f1a6e75c4a6a48429660e5a0690c935e41a
Kerby Tineus <an18200@ens.etsmtl.ca>
2016-11-05 23:01:01 -0400

    ajout de la barre de motivation et diminution de la motivation lors d'une chute

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class TestHealthBar : MonoBehaviour {
+public class PlayerStatus : MonoBehaviour {
 
 	private int maxMotivation;
 	private int currentMotivation;
@@ -11,7 +11,7 @@ public class TestHealthBar : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 		maxMotivation = 100;
 		if (PlayerPrefs.HasKey ("Motivation")) {
 			currentMotivation = PlayerPrefs.GetInt ("Motivation", currentMotivation);
@@ -19,7 +19,7 @@ public class TestHealthBar : MonoBehaviour {
 			PlayerPrefs.SetInt ("Motivation", currentMotivation);
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.R)) {
@@ -31,7 +31,14 @@ public class TestHealthBar : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.gameObject.CompareTag("Respawn")){
-			Debug.Log ("lol");
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+			Time.timeScale = 1;
+			currentMotivation = currentMotivation - 20;
+			if (currentMotivation <= 0) {
+				currentMotivation = 0;
+				GameOver ();
+			}
+			SaveMotivation ();
 		}
 	}
 	public void AjouterMotivation(int motivation){
@@ -40,24 +47,23 @@ public class TestHealthBar : MonoBehaviour {
 			if (currentMotivation >= 100) {
 				currentMotivation = 100;
 			}
-			//SaveMotivation ();
+			SaveMotivation ();
 		} else if (motivation == 2) {
 		} else {
 			currentMotivation = currentMotivation - 10;
 			if (currentMotivation <= 0) {
 				currentMotivation = 0;
-				//GameOver ();
+				GameOver ();
 			}
-
 		}
-		//SaveMotivation ();
+		SaveMotivation ();
 	}
 
 	public void SaveMotivation(){
 		PlayerPrefs.SetInt ("Motivation", currentMotivation);
 	}
-	/*
+
 	public void GameOver(){
-		
-	}*/
+		print ("Game Over");
+	}
 }
